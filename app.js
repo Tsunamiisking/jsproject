27b2bd71d723aaa25bf24dbebf93541b374d2e70
Allen Douglas <allenissabigboi@gmail.com>
2024-07-05 12:57:46 +0100

    Deplayed Leads Tracker Extension Version 1.0

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-let sum_el = document.getElementById("sum-el");
-// Create four functions: add(), subtract(), divide(), multiply()
-const Add = () => {
-    sum_el.textContent += num1 + num2
 
-}
-const Subtract = () => {
-    sum_el.textContent = "Sum: ";
-    sum_el.textContent += num1 - num2
-}
-const Multiply = () => {
-    sum_el.textContent = "Sum: ";
-    sum_el.textContent += num1 * num2
-}
-const Divide = () => {
-    sum_el.textContent = "Sum: ";
-    sum_el.textContent += num1 / num2
-}
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += `
+        <li> 
+            <a href= '${myLeads[i]}' target='_blank'>
+                ${myLeads[i]} 
+            </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems
+}
