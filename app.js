f31f8b7873341020a4910bc709ea7418a578de44
Allen Douglas <allenissabigboi@gmail.com>
2024-07-05 15:07:50 +0100

    Modified Keypress event(Possible Bug)

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
 
-inputBtn.addEventListener("keypress", (e) => {
-    if (e.key === 'Enter'){        
-        myLeads.push(inputEl.value)
-        inputEl.value = ""
-        renderLeads()
+function saveLead(e) {
+    if ((e.type === "click") || ( e.type === "keypress" && e.keyCode == 13)) {
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads();
     }
 
-})
+}
+inputBtn.addEventListener("click", saveLead)
+
+inputBtn.addEventListener("keypress", saveLead)
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         // Wrap the lead in an anchor tag (<a>) inside the <li>
         // Can you make the link open in a new tab?
+        JSON.parse(localStorage.getItem(myLeads))
+        console.log(myLeads)
         listItems += `
         <li> 
             <a href= '${myLeads[i]}' target='_blank'>
